1bfd5334e9bf7af30214d8b0fa64cde2e31246f4
sammy-code98 <sammysmart98@yahoo.com>
2023-11-28 00:16:55 +0100

    drawer

--- src/components/Transactions/FilterDrawer.tsx
+++ src/components/Transactions/FilterDrawer.tsx
@@ -1,26 +1,68 @@
 import React from 'react'
 import { MdClose } from "react-icons/md";
+import { TransactionStatus, TransactionType, btnText } from '../../utils/data';
+import Select from 'react-select';
 
-export default function FilterDrawer() {
-  const btnText = ['Today', 'Last 7 days', 'This month', 'Last 3 months']
+interface FilterDrawerProps {
+  clickHandler: () => void
+}
+
+
+export default function FilterDrawer({ clickHandler }: FilterDrawerProps) {
   // transition-transform translate-x-full
   return (
-    <div className='fixed top-0 right-0 z-50 h-screen px-8 py-4 overflow-y-auto  w-4/12 mt-2 bg-base_white rounded-3xl shadow-md'>
+    <div className='fixed top-0 right-0 z-50 h-screen px-8 py-4  overflow-y-auto  w-4/12 mt-2 bg-base_white rounded-3xl shadow-md'>
       <div className='flex justify-between items-center'>
         <h3 className='text-primary font-bold text-2xl'>Filter</h3>
-        <div className='text-base_gray cursor-pointer'>
-          <MdClose size='1.5rem' />
+        <div>
+          <MdClose size='1.5rem' onClick={clickHandler} className='cursor-pointer text-base_gray ' />
         </div>
       </div>
       <div className='mt-6 flex  justify-between items-center'>
         {btnText.map((text, index) => {
           return (
             <div key={index} >
-              <button className='py-2.5 px-4 text-center text-primary font-semibold rounded-full border border-secondary bg-base_white focus:outline-none'>{text}</button>
+              <button className='py-2.5 px-4  text-primary font-semibold rounded-full border border-secondary bg-base_white focus:outline-none'>{text}</button>
             </div>
 
           )
         })}
+      </div>
+      <div className='mt-4'>
+        <h4 className='text-primary text-base font-bold'>Date Range</h4>
+        <div className='mt-2 flex justify-between items-center'>
+          <input type='date' className='bg-secondary rounded-lg border-none px-10 py-2 text-sm text-base_gray' required />
+          <input type='date' className='bg-secondary rounded-lg border-none px-10 py-2 text-sm text-base_gray' required />
+        </div>
+      </div>
+      <div className='mt-4'>
+        <h4 className='text-primary text-base font-bold'>Transaction Type</h4>
+        <div className='mt-2'>
+          <Select isMulti name='transactions'
+            options={TransactionType}
+            className='bg-secondary rounded-lg border-none block p-3.5 w-full text-sm text-base_gray' />
+        </div>
+      </div>
+
+      <div className='mt-4'>
+        <h4 className='text-primary text-base font-bold'>Transaction Status</h4>
+        <div className='mt-2'>
+          <Select isMulti name='transactions'
+            options={TransactionStatus}
+            className='bg-secondary rounded-lg border-none block p-3.5 w-full text-sm text-base_gray' />
+        </div>
+      </div>
+
+      <div className='mt-52'>
+        <div className='flex justify-between items-center'>
+
+          <button className='py-4 px-20 text-center text-primary font-semibold rounded-full border border-secondary bg-base_white focus:outline-none'>
+            Clear
+          </button>
+          <button className='bg-primary px-20 py-4 rounded-full text-base_white font-semibold hover:opacity-50'>Apply</button>
+
+        </div>
+
       </div>
     </div>
   )
---
+++ src/utils/data.ts
@@ -0,0 +1,16 @@
+export const TransactionType = [
+  { value: "store", label: "Store Transactions" },
+  { value: "tipped", label: "Get Tipped " },
+  { value: "withdraw", label: "Withdrawals" },
+  { value: "charges", label: "Chargebacks" },
+  { value: "cash", label: "Cashbacks" },
+  { value: "referral", label: "Refer & Earn" },
+];
+
+export const TransactionStatus = [
+  { value: "sucess", label: "Successful" },
+  { value: "pending", label: "Pending" },
+  { value: "fail", label: "Failed" },
+];
+
+export const btnText = ["Today", "Last 7 days", "This month", "Last 3 months"];
